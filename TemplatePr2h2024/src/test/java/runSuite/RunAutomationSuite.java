fba26e53e24d20d11646c8f84fcada3a39ea25cd
PrathameshLad <117907892+LadPrathamesh@users.noreply.github.com>
2024-03-18 20:55:27 +0530

    Log FIx

--- TemplatePr2h2024/src/test/java/runSuite/RunAutomationSuite.java
+++ TemplatePr2h2024/src/test/java/runSuite/RunAutomationSuite.java
@@ -1,5 +1,6 @@
 package runSuite;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 import testBase.TestBase;
@@ -16,6 +17,19 @@ public class RunAutomationSuite extends TestBase {
 		} catch (InterruptedException e) {
 			e.printStackTrace(); 
 		}
+		
+		
+		String htmlContent = "<html><head><title>This is first HTML file created by Java</title></head><body>";
+        htmlContent += "<h1>New HTML baby</h1>";
+        htmlContent += "</body></html>";
+
+        try (FileWriter writer = new FileWriter("src\\test\\java\\testData\\Test Screenshots\\htmltest.html")) {
+            writer.write(htmlContent);
+            System.out.println("HTML report generated successfully at: " + "src\\test\\java\\testData\\Test Screenshots\\htmltest.html");
+        } catch (IOException e) {
+            System.err.println("Error writing HTML report to file: " + e.getMessage());
+        }
+		
 	}
 
 }
